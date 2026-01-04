979d6efed7125793c07b627f8a0d7e11031d0891
Aman Chitransh <aman.mailbox9@gmail.com>
2026-01-04 23:08:10 +0530

    java sol for longest substring pallindrome

--- src/longestPalindromeSubstring.java
+++ src/longestPalindromeSubstring.java
@@ -3,24 +3,19 @@ public class longestPalindromeSubstring {
         if (s == null || s.length() <= 1) {
             return s;
         }
-
         String lps = "";
-
         for (int i = 0; i < s.length(); i++) {
             String odd = expandAroundCenter(s, i, i);
             if (odd.length() > lps.length()) {
                 lps = odd;
             }
-
             String even = expandAroundCenter(s, i, i + 1);
             if (even.length() > lps.length()) {
                 lps = even;
             }
         }
-
         return lps;
     }
-
     private String expandAroundCenter(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
@@ -28,7 +23,6 @@ public class longestPalindromeSubstring {
         }
         return s.substring(left + 1, right);
     }
-
     public static void main(String[] args) {
         longestPalindromeSubstring longestSubstr = new longestPalindromeSubstring();
         String sol = longestSubstr.longestPalindrome("ahfyebsganaaaaaaaaaaaaauetqbsjs");
