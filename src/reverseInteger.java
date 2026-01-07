4a6dcff224447182cc8ce0fd0f57741c444a1e54
Aman Chitransh <aman.mailbox9@gmail.com>
2026-01-07 19:26:01 +0530

    reverse 32 bit integer

---
+++ src/reverseInteger.java
@@ -0,0 +1,31 @@
+public class reverseInteger {
+        public int reverse(int x) {
+            int min = -2147483648;
+            int max = 2147483647;
+
+            int res = 0;
+
+            while (x != 0) {
+                int lastDigit = x % 10;
+                x = x / 10;
+
+                if ((res > max / 10) || (res == max / 10 && lastDigit >= max % 10)) {
+                    return 0;
+                }
+                if ((res < min / 10) || (res == min / 10 && lastDigit <= min % 10)) {
+                    return 0;
+                }
+                res = (res * 10) + lastDigit;
+            }
+            return res;
+        }
+
+    public static void main(String[] args) {
+            reverseInteger reverseInteger = new reverseInteger();
+            System.out.println("25796");
+        System.out.println("Reverse of the above number is:");
+
+        System.out.println(reverseInteger.reverse(25796));
+
+    }
+}
