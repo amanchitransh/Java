09acf05fed81cbf5b2fac27645d5d475ae3ba998
Aman Chitransh <aman.mailbox9@gmail.com>
2026-01-16 23:37:56 +0530

    atoI

--- src/numPalindrome.java
+++ src/numPalindrome.java
@@ -3,9 +3,7 @@ class numPalindrome {
         if(x<0){
             return false;
         }
-
         int div =1;
-
         while(x>=10 * div){
             div = div *10;
         }
@@ -24,7 +22,7 @@ class numPalindrome {
     public static void main(String[] args) {
         numPalindrome obj = new numPalindrome();
         int[] tests = {
-                1410110141
+                1410110141,52851851,1414,1515,15151,17272,18181,19191,1478998741,18757,1785,4788
 
         };
 
