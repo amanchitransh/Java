5bc863f531c199e8ddde2e25d6df14441341d4b7
Aman Chitransh <aman.mailbox9@gmail.com>
2026-01-29 20:16:43 +0530

    2d matrix II fix

--- src/Solution.java
+++ src/Solution.java
@@ -20,7 +20,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int sol = solution.lengthOfLongestSubstring("ljnijhijbh");
+        int sol = solution.lengthOfLongestSubstring("jhgfjygfjfjhgfhjgfhjgf");
         System.out.println(sol);
     }
 }
